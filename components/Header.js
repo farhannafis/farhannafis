8b43661caa9eecb8fe33180934bc3b8697a64ca4
Farhan Nafis <farhannafis.azmali@gmail.com>
2021-06-15 00:28:15 +0800

    Fix code

--- components/Header.js
+++ components/Header.js
@@ -5,20 +5,20 @@ import DarkSwitch from './DarkSwitch';
 const Header = () => {
   return(
     <div>
-      <div class="relative p-4 sm:p-6 lg:p-8 bg-gray-50 dark:bg-coolGray-900">
-        <nav class="relative flex items-center justify-between sm:h-10 lg:justify-start" aria-label="Global">
-          <div class="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
-            <div class="flex items-center justify-end w-full md:w-auto">
-              <div class="mr-4 flex items-center text-white md:hidden">
+      <div className="relative p-4 sm:p-6 lg:p-8 bg-gray-50 dark:bg-coolGray-900">
+        <nav className="relative flex items-center justify-between sm:h-10 lg:justify-start" aria-label="Global">
+          <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
+            <div className="flex items-center justify-end w-full md:w-auto">
+              <div className="mr-4 flex items-center text-white md:hidden">
                 <DarkSwitch />
               </div>
-              <div class="-mr-2 flex items-center md:hidden">
+              <div className="-mr-2 flex items-center md:hidden">
                 <Menu as="div" className="relative inline-block text-left">
                   <div>
                     <Menu.Button className="bg-gray-200 dark:bg-coolGray-800 rounded-md p-2 inline-flex items-center justify-center text-gray-500 dark:text-primary-500 focus:outline-none focus:ring-inset focus:ring-0">
-                      <span class="sr-only">Open main menu</span>
-                      <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                      <span className="sr-only">Open main menu</span>
+                      <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
                       </svg>
                     </Menu.Button>
                   </div>
@@ -51,13 +51,13 @@ const Header = () => {
               </div>
             </div>
           </div>
-          <div class="hidden w-full md:flex md:items-center md:justify-between md:space-x-8">
-            <div class="md:space-x-8">
-              <a href="/projects" class="font-medium text-gray-500 hover:text-primary-500">Projects</a>
-              {/* <a href="/articles" class="font-medium text-gray-500 hover:text-primary-500">Articles</a> */}
-              {/* <a href="#" class="font-medium text-primary-600 hover:text-primary-500">Resume</a> */}
+          <div className="hidden w-full md:flex md:items-center md:justify-between md:space-x-8">
+            <div className="md:space-x-8">
+              <a href="/projects" className="font-medium text-gray-500 hover:text-primary-500">Projects</a>
+              {/* <a href="/articles" className="font-medium text-gray-500 hover:text-primary-500">Articles</a> */}
+              {/* <a href="#" className="font-medium text-primary-600 hover:text-primary-500">Resume</a> */}
             </div>
-            <div class="ml-auto flex items-center text-white">
+            <div className="ml-auto flex items-center text-white">
               <DarkSwitch />
             </div>
           </div>
