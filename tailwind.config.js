a8e2191d234c97151dbfaecb29ad9118eef3538e
Farhan Nafis <farhannafis.azmali@gmail.com>
2021-06-14 15:59:41 +0800

    Designing the Web

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+const colors = require('tailwindcss/colors');
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  purge: [
+    './pages/**/*.{js,ts,jsx,tsx}', 
+    './components/**/*.{js,ts,jsx,tsx}'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      ...colors,
+      primary: {
+        '100': '#cafdd5',
+        '200': '#96fbb7',
+        '300': '#61f49f',
+        '400': '#3ae995',
+        '500': '#00db88',
+        '600': '#00bc87',
+        '700': '#009d81',
+        '800': '#007f74',
+        '900': '#006769'
+      }
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      screens: {
+        'print': {
+          'raw': 'print'
+        }
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
